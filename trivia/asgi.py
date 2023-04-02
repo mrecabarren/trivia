058493a8c90ae9e5756136e491f2307370593df8
Matias Recabarren <mrecabarren@miuandes.cl>
2023-04-02 15:03:07 -0400

    * Fixing AppRegistryNotReady error

--- trivia/asgi.py
+++ trivia/asgi.py
@@ -1,9 +1,10 @@
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trivia.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trivia.settings')
 from trivia.routing import ws_urlpatterns
 from trivia_api.middlewares import JwtAuthMiddleware
 
